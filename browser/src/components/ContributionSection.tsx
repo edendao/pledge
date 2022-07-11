bf8082bb8e395929eae1abac39e39d624dcd4882
Cyrus of Eden <knouroozi@gmail.com>
2022-07-11 12:17:28 -0400

    Fix typo

--- browser/src/components/ContributionSection.tsx
+++ browser/src/components/ContributionSection.tsx
@@ -132,8 +132,8 @@ export function ContributionSection() {
                 <div className="responseContainer w-full pl-16">
                   <p className="text-lg">
                     This vision board is a participatory art piece, where you,
-                    pledge, must shares your visions of the
-                    <span className="shimmer">Eden Dao</span>.
+                    pledge, share what <span className="shimmer">Eden Dao</span>{" "}
+                    means to you.
                   </p>
                   <div className="flex">
                     <p className="text-lg pb-2">
