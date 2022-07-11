eadf09a0152b4b58176a9314490e6e56fabf01f5
Cyrus of Eden <knouroozi@gmail.com>
2022-07-11 17:43:22 -0400

    Fix Twitter

--- server/api-handlers/twitter_verify.ts
+++ server/api-handlers/twitter_verify.ts
@@ -2,21 +2,11 @@
 // must include signature in json body
 
 import { Request, Response } from "express"
-import Twitter from "twitter"
+import { TwitterApi } from "twitter-api-v2"
 
 import { Services } from "../types"
 
-const client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  bearer_token: process.env.BEARER_TOKEN,
-})
-
-const getTweetsBy = async (username: string) =>
-  await client.get("statuses/user_timeline", {
-    screen_name: username,
-    tweet_mode: "extended",
-  })
+const client = new TwitterApi(process.env.BEARER_TOKEN)
 
 // signature is their handle signed with their address
 export const verify =
@@ -24,18 +14,26 @@ export const verify =
   async (req: Request, res: Response) => {
     const { authorId, contributionId, signature } = req.body
 
+    console.time("prisma.findFirst")
     const { author, ...contribution } = await prisma.contribution.findFirst({
       where: { authorId, id: contributionId },
       include: { author: true },
     })
+    console.timeEnd("prisma.findFirst")
 
     if (Boolean(contribution.signature)) {
       res.status(200).json({ message: "Already verified!" })
       return
     }
 
-    const tweets = (await getTweetsBy(author.twitter)) as any[]
-    for (const { full_text: text } of tweets) {
+    console.time("twitter")
+    const twitterUser = await client.readOnly.v2.userByUsername(author.twitter)
+    const { tweets } = await client.readOnly.v2.userTimeline(
+      twitterUser.data.id,
+    )
+    console.timeEnd("twitter")
+
+    for (const { text } of tweets) {
       const index = text.indexOf("sig:")
       if (
         index !== -1 &&
@@ -45,10 +43,12 @@ export const verify =
             .split(" ")[0]
             .trim()
       ) {
+        console.time("prisma.update")
         await prisma.contribution.update({
           where: { id: contributionId },
           data: { signature },
         })
+        console.timeEnd("prisma.update")
 
         res.status(201).json({ message: "Verified!" })
         return
