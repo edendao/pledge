f6c557260b50a80e7e76f701bf8b82e1327787bd
Cyrus of Eden <knouroozi@gmail.com>
2022-07-12 12:45:27 -0400

    Come on Twitter

--- server/api-handlers/twitter_verify.ts
+++ server/api-handlers/twitter_verify.ts
@@ -1,5 +1,4 @@
 // POST /verify/:handle
-// must include signature in json body
 
 import { Request, Response } from "express"
 import { TwitterApi } from "twitter-api-v2"
@@ -8,7 +7,20 @@ import { Services } from "../types"
 
 const client = new TwitterApi(process.env.BEARER_TOKEN)
 
-// signature is their handle signed with their address
+const tweetsByUsername = async (username: string) => {
+  console.time("twitter")
+  try {
+    const { data: user } = await client.readOnly.v2.userByUsername(username)
+    const { tweets } = await client.readOnly.v2.userTimeline(user.id)
+    return tweets
+  } catch (error) {
+    console.error(error)
+    return []
+  } finally {
+    console.timeEnd("twitter")
+  }
+}
+
 export const verify =
   ({ prisma }: Services) =>
   async (req: Request, res: Response) => {
@@ -26,13 +38,7 @@ export const verify =
       return
     }
 
-    console.time("twitter")
-    const { tweets } = await client.readOnly.v2
-      .userByUsername(author.twitter)
-      .then(({ data: user }) => client.readOnly.v2.userTimeline(user.id))
-    console.timeEnd("twitter")
-
-    for (const { text } of tweets) {
+    for (const { text } of await tweetsByUsername(author.twitter)) {
       const index = text.indexOf("sig:")
       if (
         index !== -1 &&
@@ -42,15 +48,19 @@ export const verify =
             .split(" ")[0]
             .trim()
       ) {
-        console.time("prisma.update")
-        await prisma.contribution.update({
-          where: { id: contributionId },
-          data: { signature },
-        })
-        console.timeEnd("prisma.update")
-
-        res.status(201).json({ message: "Verified!" })
-        return
+        try {
+          console.time("prisma.update")
+          await prisma.contribution.update({
+            where: { id: contributionId },
+            data: { signature },
+          })
+          res.status(201).json({ message: "Verified!" })
+          return
+        } catch (error) {
+          res.status(500).json({ message: error.message })
+        } finally {
+          console.timeEnd("prisma.update")
+        }
       }
     }
 
